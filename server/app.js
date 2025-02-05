055fd2758d85f83e1a213a458108d45981829638
Roak kennady <roakkennady17@gmail.com>
2025-02-05 11:02:30 +0530

    Initial push

---
+++ server/app.js
@@ -0,0 +1,42 @@
+// app.js
+
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const staffDetailsRoutes = require('../server/routes/staffDetailsROutes');
+const sequelize = require('./config/db');
+const path = require('path');
+
+const app = express();
+
+// Enable CORS for all routes
+app.use(cors());
+
+// Middleware to parse JSON body
+app.use(express.json());
+
+// app.use(express.static("client/build"));
+
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')));
+}
+
+console.log(__dirname)
+console.log(path.join(__dirname, 'client/build'));
+
+
+// Middleware for logging requests
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+// Routes
+app.use('/api/staffRoutes', staffDetailsRoutes);
+
+// Sync the database and listen for requests
+sequelize.sync().then(() => {
+  app.listen(process.env.PORT, () => {
+    console.log('Listening on port', process.env.PORT);
+  });
+});
